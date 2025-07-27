f106efcd19f8ed75eb800bb7025be7b5442f9430
pawan <pawan@example.com>
2025-07-27 21:35:50 +0530

    first commit

--- settings.py
+++ settings.py
@@ -1,10 +1,15 @@
 class Settings:
     def __init__(self):       
-        self.setting_url = "https://api.example.com/data"
+        self.setting_url = "XXXX"
+        self.csv_url= "XXXX"
         self.azure_storage_connection_string = "your_connection_string"       
-
+        self.blob_sas_token = "XXX"
+        
     def get_setting_url(self):
-        return f"{self.setting_url}"
+        return f"{self.setting_url}?{self.blob_sas_token}"
     
     def get_storage_connection_string(self):
         return f"{self.azure_storage_connection_string}"
+    
+    def get_csv_url(self):
+        return f"{self.csv_url}?{self.blob_sas_token}"
