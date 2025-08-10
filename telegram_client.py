699d1aa1866bccc6414e3e777330fb378f0bc340
pawan <pawan@example.com>
2025-08-10 17:48:10 +0530

    .

--- telegram_client.py
+++ telegram_client.py
@@ -9,12 +9,15 @@ class TelegramClient:
         self.base_url = f"https://api.telegram.org/bot{self.token}/sendMessage"
 
     def send_message(self, message: str) -> str:
+        if not message or message == "{}" or not message.strip():
+            return "empty message"
+        
         payload = {
             "chat_id": self.chat_id,
             "text": message,
             "parse_mode": "HTML",  # optional
         }
-        try:
+        try:           
             response = requests.post(self.base_url, data=payload)
             response.raise_for_status()
             return "success"
